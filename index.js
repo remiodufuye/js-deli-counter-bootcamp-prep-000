feb2bcdadfd9058f2f0db3bc70db6542023334cb
remi.odufuye <remi.odufuye@gmail.com>
2019-10-04 12:41:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function takeANumber(array, name) {
  
 function takeNumbernew(array) {
   array.push(name);
-  var assignNumber = assignNumber++ ;
+  assignNumber++ ;
   var welcomeMessage 
   = `Welcome. You have ticket number ${assignNumber}` ; 
   console.log(welcomeMessage);
